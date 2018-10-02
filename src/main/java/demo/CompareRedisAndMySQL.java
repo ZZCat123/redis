30c44c75ffc1ff7ad6a6a6e6fed678f899108803
luozhen <1783921704@qq.com>
2018-10-02 22:30:34 +0800

    redis与mysql性能比较

---
+++ src/main/java/demo/CompareRedisAndMySQL.java
@@ -0,0 +1,112 @@
+package demo;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import redis.clients.jedis.Jedis;
+import utils.MySQLUtil;
+import utils.RedisConnectionFactory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Class CompareRedisAndMySQL
+ * @Description: MySQL与Redis性能对比
+ *               MySQL execute 1000 times update spend 4294ms
+ *               Redis do the same operations spend 331ms
+ *               mysql spend 12 times than redis
+ *               ---------------------------------------
+ *               MySQL execute 10000 times update spend 30379ms
+ *               Redis do the same operations spend 614ms
+ *               mysql spend 49 times than redis
+ *               ---------------------------------------
+ *               MySQL execute 100000 times update spend 253495ms
+ *               Redis do the same operations spend 4743ms
+ *               mysql spend 53 times than redis
+ *               ---------------------------------------
+ * @Author: luozhen
+ * @Create: 2018/10/02 20:37
+ */
+public class CompareRedisAndMySQL {
+
+    private static void testMySQL(int threshold) {
+        Connection connection = MySQLUtil.getConnection();
+        String sql = "update user set user_phone = user_phone + 1 where user_id = 1";
+        try {
+            PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sql);
+            for (int i = 0; i < threshold; i++) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            MySQLUtil.closeConnection(connection);
+        }
+    }
+
+    private static void testRedis(int threshold) {
+        Connection connection = MySQLUtil.getConnection();
+        Jedis jedis = RedisConnectionFactory.getConnection();
+        String sql = "select * from user where user_id = 1";
+        try {
+            PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            String username = "";
+            int userAge = 0;
+            while (resultSet.next()) {
+                username = resultSet.getString("user_name");
+                userAge = resultSet.getInt("user_age");
+            }
+            jedis.set(username, String.valueOf(userAge));
+            for (int i = 0; i < threshold; i++) {
+                jedis.incr(username);
+            }
+            userAge = Integer.parseInt(jedis.get(username));
+            jedis.flushAll();
+            sql = "update user set user_age = ? where user_id = 1";
+            preparedStatement = (PreparedStatement) connection.prepareStatement(sql);
+            preparedStatement.setInt(1, Integer.valueOf(userAge));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            RedisConnectionFactory.closeConnection();
+            MySQLUtil.closeConnection(connection);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        ExecutorService service = Executors.newCachedThreadPool();
+//        CountDownLatch latch = new CountDownLatch(threshold / 100);
+        for (int i = 1000; i >= 10; i /= 10) {
+            int threshold = 1000000;
+            threshold /= i;
+            long startTime = System.currentTimeMillis();
+//        for (int i = 0; i < threshold / 100; i++) {
+//            service.execute(() -> {
+//                CompareRedisAndMySQL.testMySQL(100);
+//                latch.countDown();
+//            });
+//        }
+//        latch.await();
+            CompareRedisAndMySQL.testMySQL(threshold);
+            long endTime = System.currentTimeMillis();
+            long mysqlSpend = endTime - startTime;
+            System.out.println("MySQL execute " + threshold + " times update spend " + mysqlSpend + "ms");
+            startTime = System.currentTimeMillis();
+//        for (int i = 0; i < 10; i++) {
+//            service.execute(() -> CompareRedisAndMySQL.testRedis(threshold));
+//        }
+            CompareRedisAndMySQL.testRedis(threshold);
+            endTime = System.currentTimeMillis();
+            long redisSpend = endTime - startTime;
+            System.out.println("Redis do the same operations spend " + redisSpend + "ms");
+            System.out.println("mysql spend " + mysqlSpend / redisSpend + " times than redis");
+            System.out.println("---------------------------------------");
+//        service.shutdown();
+        }
+    }
+}
---
+++ src/main/java/utils/MySQLUtil.java
@@ -0,0 +1,71 @@
+package utils;
+
+import com.mysql.jdbc.Connection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @Class MySQLUtil
+ * @Description: TODO
+ * @Author: luozhen
+ * @Create: 2018/10/02 20:37
+ */
+public class MySQLUtil {
+
+    private static String jdbcConfig;
+
+    static {
+        jdbcConfig = "jdbc.properties";
+    }
+
+    public static Connection getConnection() {
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+        Connection connection = null;
+        try {
+            inputStream = MySQLUtil.class.getClassLoader().getResourceAsStream(jdbcConfig);
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("load jdbc.properties file failed!");
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String driver = properties.getProperty("jdbc.driver");
+        String url = properties.getProperty("jdbc.url");
+        String username = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
+        try {
+            Class.forName(driver);
+            connection = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Can not find driver!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("close MySQL connection failed!");
+        }
+    }
+
+}
